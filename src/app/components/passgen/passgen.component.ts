4478cd155f98d3c53fa07e417625ee5c62b3da91
mostafa mirbabaei <mirbabaei.mostafa@gmail.com>
2025-02-04 00:26:53 +0330

    Some update

--- src/app/components/passgen/passgen.component.ts
+++ src/app/components/passgen/passgen.component.ts
@@ -8,6 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './passgen.component.css',
 })
 export class PassgenComponent {
+  passLenght: number = 1;
+  generatedPass: string = 'Pass';
+  isLetters: boolean = false;
+  isNumbers: boolean = false;
+  isSymboles: boolean = false;
+
+  handleChange(e: string) {
+    const parseInput = parseInt(e.value);
+    if (!isNaN(parseInput)) {
+      this.passLenght = parseInput;
+    }
+    console.log(this.passLenght)
+  }
+
+  handleLetterCheck() {
+    this.isLetters = !this.isLetters;
+  }
+
+  handleNumberCheck() {
+    this.isNumbers = !this.isNumbers;
+  }
+
+  handleSymboleCheck() {
+    this.isSymboles = !this.isSymboles;
+  }
+
   onGenerateClick() {
     console.log('Generate');
   }
